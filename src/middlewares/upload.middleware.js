5e88951e26fe4b0e4004030c83f5159c2e693790
Bikash Nepali <benzaminbikash@gmail.com>
2024-08-15 08:08:11 +0545

    change middleware

--- src/middlewares/upload.middleware.js
+++ src/middlewares/upload.middleware.js
@@ -1,9 +1,17 @@
 const path = require("path");
 const multer = require("multer");
+const fs = require("fs");
+
+const uploadsDir = path.join("/tmp", "uploads");
+
+// Create the uploads directory if it doesn't exist
+if (!fs.existsSync(uploadsDir)) {
+  fs.mkdirSync(uploadsDir);
+}
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "uploads");
+    cb(null, uploadsDir);
   },
   filename: function (req, file, cb) {
     cb(
