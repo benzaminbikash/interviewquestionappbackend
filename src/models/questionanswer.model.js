1ec62f4a18ec929b9f3107ae74e0c2cf0b682e98
Bikash Nepali <benzaminbikash@gmail.com>
2024-08-26 21:26:02 +0545

    remore required

--- src/models/questionanswer.model.js
+++ src/models/questionanswer.model.js
@@ -13,11 +13,9 @@ var questionanswerSchema = new mongoose.Schema(
   {
     question: {
       type: String,
-      required: true,
     },
     answer: {
       type: [String],
-      required: true,
     },
     points: POINT,
     image: {
