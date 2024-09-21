e50091f204eec6f555235332aa8cddab212dd54f
Bikash Nepali <benzaminbikash@gmail.com>
2024-09-21 09:12:23 +0545

    update email configure

--- src/utils/emailConfigure.js
+++ src/utils/emailConfigure.js
@@ -3,8 +3,8 @@ const nodemailer = require("nodemailer");
 const emailConfig = async (email, subject, text) => {
   let transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
+    port: process.env.EMAIL_PORT,
+    secure: false,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
